0aed70d327d605c4f7175657d3610b59adb5aa08
Amanturov7 <edi.amanturov@gmail.com>
2024-04-03 12:43:52 +0600

    email

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './FeedbackForm.css'; // Подключаем файл стилей
+import emailjs from '@emailjs/browser';
+import './FeedbackForm.css'; 
 
 class FeedbackForm extends Component {
     constructor(props) {
@@ -19,56 +20,67 @@ class FeedbackForm extends Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
-        // Здесь вы можете добавить логику отправки данных на сервер или другую обработку формы
-        console.log('Отправлено:', this.state);
+
+        emailjs.sendForm('service_iv4fujq', 'template_kafemik', e.target, 'MA_tIBIhJOCzuuUes')
+            .then((result) => {
+                console.log('Email successfully sent:', result.text);
+            }, (error) => {
+                console.error('Error sending email:', error.text);
+            });
+
+        this.setState({
+            name: '',
+            email: '',
+            message: '',
+        });
     }
 
     render() {
         return (
-          <section  id="contact"> 
-            <div className="feedback-container">
-                <div className="feedback-text">
-                    <h2>Свяжитесь с нами</h2>
-                    <p>Если у вас есть вопросы или комментарии, пожалуйста, заполните форму ниже.</p>
+            <section id="contact">
+                <div className="feedback-container">
+                    <div className="feedback-text">
+                        <h2>Свяжитесь с нами</h2>
+                        <p>Если у вас есть вопросы или комментарии, пожалуйста, заполните форму ниже.</p>
+                    </div>
+                    <div className="feedback-form">
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="form-group">
+                                <label htmlFor="name">Имя</label>
+                                <input
+                                    type="text"
+                                    id="name"
+                                    name="name"
+                                    value={this.state.name}
+                                    onChange={this.handleChange}
+                                    required
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="email">Email</label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    name="email"
+                                    value={this.state.email}
+                                    onChange={this.handleChange}
+                                    required
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="message">Сообщение</label>
+                                <textarea
+                                    id="message"
+                                    name="message"
+                                    value={this.state.message}
+                                    onChange={this.handleChange}
+                                    required
+                                />
+                            </div>
+                            <button className='btn btn-primary btn-lg js-scroll-trigger' type="submit">Отправить</button>
+                        </form>
+                    </div>
                 </div>
-                <div className="feedback-form">
-                    <form onSubmit={this.handleSubmit}>
-                        <div className="form-group">
-                            <label htmlFor="name">Имя:</label>
-                            <input
-                                type="text"
-                                id="name"
-                                name="name"
-                                value={this.state.name}
-                                onChange={this.handleChange}
-                                required
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="email">Email</label>
-                            <input
-                                type="email"
-                                id="email"
-                                name="email"
-                                value={this.state.email}
-                                onChange={this.handleChange}
-                                required
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="message">Сообщение</label>
-                            <textarea
-                                id="message"
-                                name="message"
-                                value={this.state.message}
-                                onChange={this.handleChange}
-                                required
-                            />
-                        </div>
-                        <button className='btn btn-primary btn-lg js-scroll-trigger' type="submit">Отправить</button>
-                    </form>
-                </div>
-            </div>
             </section>
         );
     }
