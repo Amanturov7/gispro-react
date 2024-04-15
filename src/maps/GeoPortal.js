9727e5618210e70453ba217d4855e06ced03549a
Amanturov7 <edi.amanturov@gmail.com>
2024-04-15 13:22:03 +0600

    style geoportal

--- src/maps/GeoPortal.js
+++ src/maps/GeoPortal.js
@@ -6,21 +6,46 @@ import { OSM, TileWMS } from 'ol/source';
 import { fromLonLat } from 'ol/proj';
 import FormControl from 'react-bootstrap/FormControl';
 import MyNavbar from '../components/MyNavbar';
+import FooterSection from '../components/footer2';
+
 import "./GeoPortal.css";
 
 const GeoPortal = () => {
     const [darkMap, setDarkMap] = useState(false);
-    const [selectedSource, setSelectedSource] = useState('');
+    const [selectedSource, setSelectedSource] = useState('http://planting.caiag.kg/cgi-bin/mapserv.cgi?map=wms.map'); // Выбранный источник данных WMS по умолчанию
     const [selectedLayers, setSelectedLayers] = useState([]);
     const [layerVisibility, setLayerVisibility] = useState({});
-
     const mapRef = useRef(null); // Ссылка на карту
 
     // Информация об источниках данных WMS
     const wmsSources = {
-        'http://planting.caiag.kg/cgi-bin/mapserv.cgi?map=wms.map': ['aimak_en', 'plantedpointadm', 'oblast_en', 'plantinglot'],
-        'http://localhost/geoserver/geonode/wms': ['geonode:batken', 'layer2', 'layer3'],
-        'https://kyrgyzstan.sibelius-datacube.org:5000/': ['ModisAnomaly','ModisRGB']
+        'http://planting.caiag.kg/cgi-bin/mapserv.cgi?map=wms.map': {
+            name: 'http://planting.caiag.kg/cgi-bin/mapserv.cgi?map=wms.map',
+            label: 'Planting caiag',
+            layers: [
+                { name: 'aimak_en', label: 'Аймаки' },
+                { name: 'plantedpointadm', label: 'Точки посадки' },
+                { name: 'oblast_en', label: 'Области' },
+                { name: 'plantinglot', label: 'Земельные участки' }
+            ]
+        },
+        'http://localhost/geoserver/geonode/wms': {
+            name: 'http://localhost/geoserver/geonode/wms',
+            label: 'Gispro',
+            layers: [
+                { name: 'geonode:batken', label: 'Баткен' },
+                { name: 'layer2', label: 'Слой 2' },
+                { name: 'layer3', label: 'Слой 3' }
+            ]
+        },
+        'https://kyrgyzstan.sibelius-datacube.org:5000/': {
+            name: 'https://kyrgyzstan.sibelius-datacube.org:5000/',
+            label: 'Sibelius',
+            layers: [
+                { name: 'ModisAnomaly', label: 'Аномалии Modis' },
+                { name: 'ModisRGB', label: 'RGB Modis' }
+            ]
+        }
     };
 
     useEffect(() => {
@@ -34,16 +59,16 @@ const GeoPortal = () => {
     }, [selectedLayers, layerVisibility]);
 
     const fetchLayers = async (source) => {
-        const layers = wmsSources[source];
+        const { layers } = wmsSources[source];
         setSelectedLayers(layers);
 
-        const initialVisibility = layers.reduce((acc, layerName) => {
-            acc[layerName] = true;
+        const initialVisibility = layers.reduce((acc, { name }) => {
+            acc[name] = false; // Устанавливаем все слои выключенными по умолчанию
             return acc;
         }, {});
         setLayerVisibility(initialVisibility);
 
-        // Если карта еще не создана, создаем ее
+        // Создаем карту, если она еще не создана
         if (!mapRef.current) {
             const baseLayer = darkMap ?
                 new TileLayer({
@@ -78,13 +103,13 @@ const GeoPortal = () => {
         });
 
         // Добавляем новые слои WMS
-        layers.forEach(layerName => {
+        layers.forEach(({ name, label }) => {
             const tileWMS = new TileWMS({
                 url: source,
-                params: { 'LAYERS': layerName, 'TILED': true },
+                params: { 'LAYERS': name, 'TILED': true },
                 serverType: 'geoserver'
             });
-            const tileLayer = new TileLayer({ source: tileWMS, visible: layerVisibility[layerName] });
+            const tileLayer = new TileLayer({ source: tileWMS, visible: false }); // Все слои WMS изначально выключены
             map.addLayer(tileLayer);
         });
     };
@@ -97,8 +122,8 @@ const GeoPortal = () => {
         setSelectedSource(event.target.value);
     };
 
-    const handleWmsLayerToggle = (layerName) => {
-        const updatedVisibility = { ...layerVisibility, [layerName]: !layerVisibility[layerName] };
+    const handleWmsLayerToggle = (name) => {
+        const updatedVisibility = { ...layerVisibility, [name]: !layerVisibility[name] };
         setLayerVisibility(updatedVisibility);
     };
 
@@ -107,8 +132,8 @@ const GeoPortal = () => {
         if (map) {
             map.getLayers().forEach(layer => {
                 if (layer instanceof TileLayer && layer.getSource() instanceof TileWMS) {
-                    const layerName = layer.getSource().getParams()['LAYERS'];
-                    layer.setVisible(layerVisibility[layerName]);
+                    const name = layer.getSource().getParams()['LAYERS'];
+                    layer.setVisible(layerVisibility[name]);
                 }
             });
         }
@@ -117,25 +142,27 @@ const GeoPortal = () => {
     return (
         <div>
             <MyNavbar />
-            <div className="container">
+            <div className="container-geo">
                 <div className="wms-selector">
-                    <FormControl as="select" value={selectedSource} onChange={handleSourceChange}>
+                <h5>Выберите источник данных</h5>
+
+                    <FormControl  className="" as="select" value={selectedSource} onChange={handleSourceChange}>
                         <option value="">Выберите источник данных WMS</option>
                         {Object.keys(wmsSources).map(source => (
-                            <option key={source} value={source}>{source}</option>
+                            <option key={source} value={source}>{wmsSources[source].label}</option>
                         ))}
                     </FormControl>
                     {selectedLayers.length > 0 && (
                         <div>
-                            <h5>Выберите слои:</h5>
-                            {selectedLayers.map(layer => (
-                                <div key={layer}>
+                            <h5>Выберите слои</h5>
+                            {selectedLayers.map(({ name, label }) => (
+                                <div key={name}>
                                     <input
                                         type="checkbox"
-                                        checked={layerVisibility[layer]}
-                                        onChange={() => handleWmsLayerToggle(layer)}
+                                        checked={layerVisibility[name]}
+                                        onChange={() => handleWmsLayerToggle(name)}
                                     />
-                                    <label>{layer}</label>
+                                    <label>{label}</label>
                                 </div>
                             ))}
                         </div>
@@ -145,6 +172,8 @@ const GeoPortal = () => {
                     <div id="map"></div>
                 </div>
             </div>
+            <FooterSection />
+
         </div>
     );
 };
